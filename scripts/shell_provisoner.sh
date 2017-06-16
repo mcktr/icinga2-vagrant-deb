837350aee6309bb10da6d83e09c8c5ab9853aa6c
mcktr <mcktr55@gmail.com>
2017-06-16 19:19:20 +0200

    WIP: Using ansible-pull for provisioning
    refs #7

--- scripts/shell_provisoner.sh
+++ scripts/shell_provisoner.sh
@@ -3,6 +3,17 @@
 set -e
 
 echo "Running initial upgrade"
-apt-get update
-apt-get -y dist-upgrade
-
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+echo "Finished initial upgrade"
+echo "Installing pip"
+sudo apt-get install -y -qq build-essential curl git libssl-dev libffi-dev python-dev
+curl -s https://bootstrap.pypa.io/get-pip.py | sudo python
+echo "Finished installing pip"
+echo "Installing Ansible"
+sudo pip -q install ansible
+echo "Finished installing Ansible"
+sudo mkdir /vagrant/ansible
+echo "Running provisioning"
+ansible-pull -U https://github.com/mcktr/ansible-icinga2.git -C feature/ansible-pull -d /vagrant/ansible
+echo "Finished provisioning"
